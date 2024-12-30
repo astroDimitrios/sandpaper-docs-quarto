4e19ab69a55ba0290d4aaf5c365be0d167effd1d
Dimitrios Theodorakis <astrodimitrios@gmail.com>
2024-12-30 12:56:09 +0000

    Fix bug if spaces are present in the quarto output path for the bash script

--- _extensions/astrodimitrios/carpentries/post-process-html.sh
+++ _extensions/astrodimitrios/carpentries/post-process-html.sh
@@ -1,9 +1,9 @@
 # Remove Quarto's bootstrap
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/bootstrap\/bootstrap/d'
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/quarto-html/d'
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/clipboard/d'
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/quarto-nav/d'
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/quarto-search/d'
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*.html" -print0 | xargs -0 sed -i '/styles.css.map/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/bootstrap\/bootstrap/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/quarto-html/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/clipboard/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/quarto-nav/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*.html" -print0 | xargs -0 sed -i '/..\/site_libs\/quarto-search/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*.html" -print0 | xargs -0 sed -i '/styles.css.map/d'
 
-find $QUARTO_PROJECT_OUTPUT_DIR -name "*extract_all_images.html" -print0 | xargs -0 sed -i '/<script id="quarto-html-after-body"/,/<\/script>/d'
+find "$QUARTO_PROJECT_OUTPUT_DIR" -name "*extract_all_images.html" -print0 | xargs -0 sed -i '/<script id="quarto-html-after-body"/,/<\/script>/d'
